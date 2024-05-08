980e9200230c0367f175cbff6836e3a6d34dfc1b
EdiedRamos <edieisa22@gmail.com>
2024-05-08 15:58:55 -0500

    style: implement header navbar and access mobile version

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,24 +1,15 @@
+import "./Header.scss";
+
 import { Button, Logo } from "@/Components";
 
 const NAV_MOCK = ["Features", "Pricing", "Resources"];
 
 export const Header = () => {
   return (
-    <header>
+    <header className="header">
       <Logo />
-      <nav>
-        <ul>
-          {NAV_MOCK.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </nav>
-      <div>
-        <Button>Login</Button>
-        <Button>Sign Up</Button>
-      </div>
-      {/* mobile version */}
-      <div>
+      {/* desktop version */}
+      <div className="header__container--desktop">
         <nav>
           <ul>
             {NAV_MOCK.map((item) => (
@@ -27,6 +18,21 @@ export const Header = () => {
           </ul>
         </nav>
         <div>
+          <Button>Login</Button>
+          <Button>Sign Up</Button>
+        </div>
+      </div>
+      {/* mobile version */}
+      <div className="header__container--mobile container">
+        <nav className="header__navbar--mobile">
+          <ul>
+            {NAV_MOCK.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </nav>
+        <hr className="header__container--separator" />
+        <div className="header__access--mobile">
           <Button>Login</Button>
           <Button>Sign Up</Button>
         </div>
