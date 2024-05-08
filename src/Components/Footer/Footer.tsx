682f9586b0e12d1a9b0f66290b7a6de644fb7e5c
EdiedRamos <edieisa22@gmail.com>
2024-05-07 22:01:10 -0500

    chore: add container breakpoints

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,26 +4,28 @@ import { Listing, Logo, SocialMedia } from "@/Components";
 
 export const Footer = () => {
   return (
-    <footer className="footer footer__container">
-      <div className="footer__logo">
-        <Logo color="light" />
-      </div>
-      <div className="footer__redirects">
-        <section className="footer__sections">
-          <Listing
-            title="Features"
-            items={["Link Shortening", "Branded Links", "Analytics"]}
-          />
-          <Listing
-            title="Resources"
-            items={["Blog", "Developers", "Support"]}
-          />
-          <Listing
-            title="Company"
-            items={["About", "Our Team", "Careers", "Contact"]}
-          />
-        </section>
-        <SocialMedia />
+    <footer className="footer">
+      <div className="footer__container container">
+        <div className="footer__logo">
+          <Logo color="light" />
+        </div>
+        <div className="footer__redirects">
+          <section className="footer__sections">
+            <Listing
+              title="Features"
+              items={["Link Shortening", "Branded Links", "Analytics"]}
+            />
+            <Listing
+              title="Resources"
+              items={["Blog", "Developers", "Support"]}
+            />
+            <Listing
+              title="Company"
+              items={["About", "Our Team", "Careers", "Contact"]}
+            />
+          </section>
+          <SocialMedia />
+        </div>
       </div>
     </footer>
   );
