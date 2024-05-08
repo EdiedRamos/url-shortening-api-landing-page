9ab931d4b0e8bf4656cd2bd046af06a3f2feffeb
EdiedRamos <edieisa22@gmail.com>
2024-05-08 17:51:18 -0500

    style: implement hero design

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,20 +1,22 @@
+import "./Hero.scss";
+
 import { Button } from "@/Components";
 import { WorkingIcon } from "@/Resources/Images";
 
 export const Hero = () => {
   return (
-    <section>
-      <article>
-        <h1>More than just shorter links</h1>
-        <p>
+    <section className="hero hero__container container">
+      <div className="hero__img">
+        <img src={WorkingIcon} alt="working icon" />
+      </div>
+      <article className="hero__group">
+        <h1 className="hero__title">More than just shorter links</h1>
+        <p className="hero__description">
           Build your brand's recognition and get detailed insights on how your
           links are performing.
         </p>
         <Button>Get Started</Button>
       </article>
-      <div>
-        <img src={WorkingIcon} alt="working icon" />
-      </div>
     </section>
   );
 };
