afbdd3ea07002650c673ed164c3fffa33d709683
EdiedRamos <edieisa22@gmail.com>
2024-05-09 00:39:45 -0500

    chore: remove mock data

--- src/Components/ShortedLinks/ShortedLinks.tsx
+++ src/Components/ShortedLinks/ShortedLinks.tsx
@@ -3,25 +3,12 @@ import "./ShortedLinks.scss";
 import { ShortedLink } from "../ShortedLink/ShortedLink";
 import { useShortener } from "@/Hooks";
 
-const MOCK = [
-  {
-    id: "1",
-    originalLink: "https://www.frontendmentor.io",
-    shortedLink: "https://rel.ink/k4Ikzl",
-  },
-  {
-    id: "2",
-    originalLink: "https://www.youtube.com",
-    shortedLink: "https://rel.ink/z5Ilsl",
-  },
-];
-
 export const ShortedLinks = () => {
-  const shortener = useShortener();
+  const { linkResults } = useShortener();
   return (
     <section className="shorted-links">
       <div className="shorted-links__container container">
-        {MOCK.map((info) => (
+        {linkResults.map((info) => (
           <ShortedLink key={info.id} {...info} />
         ))}
       </div>
