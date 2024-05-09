96284684b0dd2e5ea54eb51d2f94cd7ebecabb55
EdiedRamos <edieisa22@gmail.com>
2024-05-08 23:18:37 -0500

    fix: update svg tags name according to react naming

--- src/Generals/Icons/CloseIcon.tsx
+++ src/Generals/Icons/CloseIcon.tsx
@@ -7,20 +7,20 @@ export const CloseIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
         <path
           d="M6 6L18 18M18 6L6 18"
           stroke="#000000"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>{" "}
       </g>
     </svg>
