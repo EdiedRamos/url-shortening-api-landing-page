26f621c80b16f1c6a0e81db9baae08186632d1a6
EdiedRamos <edieisa22@gmail.com>
2024-05-08 22:31:21 -0500

    chore: implement shortener structure

--- src/Components/Shortener/Shortener.tsx
+++ src/Components/Shortener/Shortener.tsx
@@ -1,11 +1,17 @@
+import "./Shortener.scss";
+
 import { Button } from "@/Components";
 
 export const Shortener = () => {
   return (
-    <section>
-      <div>
-        <input placeholder="Shorten a link here..." type="text" />
-        <Button>Shorten It!</Button>
+    <section className="shortener">
+      <div className="shortener__container">
+        <input
+          className="shortener__input"
+          placeholder="Shorten a link here..."
+          type="text"
+        />
+        <Button className="shortener__button">Shorten It!</Button>
       </div>
     </section>
   );
